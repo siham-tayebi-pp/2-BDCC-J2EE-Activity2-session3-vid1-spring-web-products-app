63e6264693a576f226cc31e502bb4322be5f2dba
siham-tayebi-pp <siham.tayebi-etu@etu.univh2c.ma>
2026-04-18 13:02:29 +0100

    Second commit

--- src/main/java/net/tayebi/webspringmvcproductsapplication/web/ProductRestAPI.java
+++ src/main/java/net/tayebi/webspringmvcproductsapplication/web/ProductRestAPI.java
@@ -3,15 +3,13 @@ package net.tayebi.webspringmvcproductsapplication.web;
 import net.tayebi.webspringmvcproductsapplication.entities.Product;
 import net.tayebi.webspringmvcproductsapplication.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @CrossOrigin("http://localhost:4200") // jautorise ce domaine a menvoyer des req
-public class ProductRestController {
+public class ProductRestAPI {
     @Autowired
     private ProductRepository productRepository;
 
